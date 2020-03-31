5dab796fda0e7b67c887782f2b7048bf8cba934a
Daniel Gutierrez <daniel.r.gutierrez.94@gmail.com>
2020-03-31 12:33:50 -0700

    Add placeholder methods

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode2019Day3
 {
     class Program
     {
+        private const int SIZE = 1000;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] grid = new int[SIZE,SIZE];
+            DrawWires(ref grid);
+            Console.WriteLine($"Part 1: distance = {FindShortestIntersection(ref grid)}");
         }
+
+        static int FindShortestIntersection(ref int[,] grid)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void DrawWires(ref int[,] grid)
+        {
+            throw new NotImplementedException();
+        }
+
+        
     }
 }
